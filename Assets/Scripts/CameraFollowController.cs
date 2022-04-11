664816864f6396d377fc71264bde9df67425eefe
tomcatmew <helltomcat@gmail.com>
2022-04-11 18:43:43 +0900

    first commit

---
+++ Assets/Scripts/CameraFollowController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowController : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public Vector3 offset = new Vector3(0.0f, 1.0f, -1.7f);
+    public float followSpeed = 10;
+    public float lookSpeed = 75;
+    public void LookAtTarget()
+    {
+        Vector3 _lookDirection = objectToFollow.position - transform.position;
+        Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
+        transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
+    }
+
+    public void MoveToTarget()
+    {
+        Vector3 _targetPos = objectToFollow.position +
+                             objectToFollow.forward * offset.z +
+                             objectToFollow.right * offset.x +
+                             objectToFollow.up * offset.y;
+        transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
+
+    }
+
+    private void Update()
+    {
+        LookAtTarget();
+        MoveToTarget();
+    }
+
+}
